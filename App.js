aa915a723ec1329db7147a1587aec2cb3628f4e8
Saikat Das <saikatdas0790@gmail.com>
2019-01-07 15:01:19 +0530

    Linking vs WebBrowser

--- App.js
+++ App.js
@@ -1,62 +1,42 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading, Asset, Font, Icon } from 'expo';
-import AppNavigator from './navigation/AppNavigator';
+import React, { Component } from "react";
+import { Button, Linking, View, StyleSheet } from "react-native";
+import { Constants, WebBrowser } from "expo";
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false,
-  };
-
+export default class App extends Component {
   render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
+    return (
+      <View style={styles.container}>
+        <Button
+          title="Open URL with ReactNative.Linking"
+          onPress={this.handleOpenWithLinking}
+          style={styles.button}
         />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+        <Button
+          title="Open URL with Expo.WebBrowser"
+          onPress={this.handleOpeningWithWebBrowser}
+          style={styles.button}
+        />
+      </View>
+    );
   }
 
-  _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
-      Font.loadAsync({
-        // This is the font that we are using for our tab bar
-        ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-      }),
-    ]);
+  handleOpenWithLinking = () => {
+    Linking.openURL("https://expo.io");
   };
 
-  _handleLoadingError = error => {
-    // In this case, you might want to report the error to your error
-    // reporting service, for example Sentry
-    console.warn(error);
-  };
-
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
+  handleOpeningWithWebBrowser = () => {
+    WebBrowser.openBrowserAsync("https://expo.io");
   };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    alignItems: "center",
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: "#ecf0f1"
   },
+  button: {
+    marginVertical: 10
+  }
 });
---
+++ screens/01-AnchorExample.js
@@ -0,0 +1,16 @@
+import { Linking, Text } from "react-native";
+
+export default class Anchor extends React.Component {
+  handlePress = () => {
+    Linking.openURL(this.props.href);
+    this.props.onPress && this.props.onPress();
+  };
+
+  render() {
+    return (
+      <Text {...this.props} onPress={this.handlePress}>
+        {this.props.children}
+      </Text>
+    );
+  }
+}
